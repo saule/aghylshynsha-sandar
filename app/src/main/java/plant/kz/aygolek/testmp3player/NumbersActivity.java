46ac51093257a6bee51c54b19931c1d6dc0a98a9
saule <saule2007@gmail.com>
2018-05-28 12:06:03 +0300

    Initial commit

---
+++ app/src/main/java/plant/kz/aygolek/testmp3player/NumbersActivity.java
@@ -0,0 +1,39 @@
+package plant.kz.aygolek.testmp3player;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class NumbersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Create a list of words
+      /*  ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word("one", "lutti", R.drawable.number_one));
+        words.add(new Word("two", "otiiko", R.drawable.number_two));
+        words.add(new Word("three", "tolookosu", R.drawable.number_one));
+        words.add(new Word("four", "oyyisa", R.drawable.number_two));
+        words.add(new Word("five", "massokka", R.drawable.number_one));
+        words.add(new Word("six", "temmokka", R.drawable.number_two)); */
+
+       /* ArrayList<String> words = new ArrayList<>();
+        words.add("one");
+        words.add("oned");
+        words.add("one4");
+
+
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,words);
+
+        ListView listView = findViewById(R.id.list_numbers);
+        listView.setAdapter(arrayAdapter);*/
+
+    }
+}
